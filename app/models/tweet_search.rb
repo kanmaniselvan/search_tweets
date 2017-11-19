2cdc70b303c16a9fc541a728b76a1fee20f0db0e
kanmani <kanmaniselvan.m@in.reportbee.com>
2017-11-19 11:37:42 +0530

    Implement loading of tweets based on the search query
    - Add dot-env and oauth gem for saving the developer oauth credentials and making request to the twitter search API.
    - Remove stubbed values in the search method to return the actual tweet results.
    - Format HTML to render the tweets nicely.
    - Remove byebug_history file from git.
    - Add placeholder for search input.

--- app/models/tweet_search.rb
+++ app/models/tweet_search.rb
@@ -1,10 +1,21 @@
 class TweetSearch
+  TWITTER_SEARCH_API = 'https://api.twitter.com/1.1/search/tweets.json'
+
   def initialize(search_query)
     @search_query = search_query
   end
 
   def search
-    # Stubbing to return sample tweets for now
-    [{tweet: 'Tweet 1'}, {tweet: 'Tweet 2'}, {tweet: 'Tweet 3'}]
+    response = access_token.get("#{TWITTER_SEARCH_API}?q=#{@search_query}")
+    JSON.parse(response.body)['statuses']
+  end
+
+  private
+  def consumer
+    @consumer ||= OAuth::Consumer.new(ENV['CONSUMER_KEY'], ENV['CONSUMER_SECRET'])
+  end
+
+  def access_token
+    @access_token = OAuth::AccessToken.new(consumer, ENV['ACCESS_TOKEN'], ENV['ACCESS_TOKEN_SECRET'])
   end
 end
